b9d55bf962be4267fe2eb259f7119eda4f231f77
Gamelink2 <nick1klaassen@gmail.com>
2023-09-26 12:35:31 +0200

    fixed scrollbar funcionality with navbar

--- src/Components/Navigation_Bar.tsx
+++ src/Components/Navigation_Bar.tsx
@@ -11,7 +11,7 @@ import About from './aboutMe';
 import Home from './homePage';
 import Projects from './projects';
 import Contact from './contact';
-import {useState, useEffect} from "react";
+import {useState, useEffect, useRef} from "react";
 import SimpleBar from 'simplebar-react';
 import 'simplebar-react/dist/simplebar.min.css';
 
@@ -19,6 +19,7 @@ function NavBar() {
   const [activeLink, setActiveLink] = useState('/');
   const [scrolled, setScrolled] = useState(false);
   const [expanded, setExpanded] = useState(false);
+  const scrollableNodeRef = useRef<HTMLDivElement | null>(null); // Set the expected type
 
   const handleToggle = () => {
     setExpanded(!expanded); // Toggle expanded state when Toggler is clicked
@@ -26,24 +27,31 @@ function NavBar() {
 
   useEffect(() => {
     const onScroll = () => {
-      if (window.scrollY > 50){
-      setScrolled(true);
+      if (scrollableNodeRef.current && scrollableNodeRef.current.scrollTop > 50) {
+        setScrolled(true);
       } else {
         setScrolled(false);
       }
+    };
+    
+    if (scrollableNodeRef.current) { // Check if it exists before adding the event listener
+      scrollableNodeRef.current.addEventListener("scroll", onScroll);
     }
+  
+    return () => {
+      if (scrollableNodeRef.current) { // Check if it exists before removing the event listener
+        scrollableNodeRef.current.removeEventListener("scroll", onScroll);
+      }
+    };
+  }, []);
 
-    window.addEventListener("scroll", onScroll);
-
-    return () => window.removeEventListener("scroll", onScroll);
-  }, [])
   const onUpdateActiveLink = (value:any) => {
     setActiveLink(value);
   }
 
   return (
     <BrowserRouter>
-    <SimpleBar style={{ maxHeight: '100%', height: 'calc(100vh - 0px)' }}>
+    <SimpleBar style={{ maxHeight: '100%', height: '100vh' }} scrollableNodeProps={{ ref: scrollableNodeRef }}>
     <Navbar className={`navbar ${scrolled ? 'scrolled' : ''} ${expanded ? 'expanded' : ''}`} sticky="top" expand="md">
       <Container>
         <Navbar.Brand as={Link} to="/"><img src={Logo} alt="Portfolio Nick Klaassen" /> </Navbar.Brand>
@@ -57,8 +65,8 @@ function NavBar() {
           <span className="navbar-text">
             <div className="social-icon">
               <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUjcmljayBhc3RsZXkgbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D"><img src={IconLinkedIn} alt= "" /></a>
-              <a href="/"><img src={IconInsta} alt= "" /></a>
-              <a href="/"><img src={IconShop} alt= "" /></a>
+              <a href="https://www.instagram.com/"><img src={IconInsta} alt= "" /></a>
+              <a href="https://www.etsy.com"><img src={IconShop} alt= "" /></a>
             </div>
             <div className="connect-button">      
             <Nav.Link onClick={() => onUpdateActiveLink('/Contact')} as={Link} to="/Contact" ><span className="text">Contact me</span></Nav.Link>
